4489a459a5a9c274cb109f656f54bfd1e51b43e7
nabillawr <nabillawr15@gmail.com>
2019-03-25 18:29:25 +0700

    taxi

---
+++ taxi/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace LatianClassDanObject
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Taxi taxi = new Taxi();
+
+
+
+            taxi.DriverName = "Kiki";
+
+            taxi.OnDuty = false;
+
+            taxi.NumPassenger = 10;
+
+
+
+            taxi.TaxiInfo();
+
+            taxi.PickUpPassenger();
+
+            taxi.DropOfPassenger();
+
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
